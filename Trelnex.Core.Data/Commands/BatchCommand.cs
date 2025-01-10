23bf08567ce57ffa195933a86dd1318ea746a685
Steven Kehrli <stevenkehrli@live.com>
2025-01-10 00:15:16 -0800

    code complete - need to test

--- Trelnex.Core.Data/Commands/BatchCommand.cs
+++ Trelnex.Core.Data/Commands/BatchCommand.cs
@@ -53,7 +53,7 @@ internal class BatchCommand<TInterface, TItem>(
     private readonly SemaphoreSlim _semaphore = new(1, 1);
 
     /// <summary>
-    /// The batch of items to save.
+    /// The batch of save commands to save.
     /// </summary>
     private readonly List<SaveCommand<TInterface, TItem>> _saveCommands = [];
 
@@ -97,12 +97,15 @@ internal class BatchCommand<TInterface, TItem>(
         CancellationToken cancellationToken)
     {
         // ensure that only one operation that modifies the batch is in progress at a time
-        _semaphore.Wait(cancellationToken);
+        await _semaphore.WaitAsync(cancellationToken);
 
         try
         {
-            // allocate the array of batch results
-            var batchResults = new BatchResult<TInterface, TItem>[_saveCommands.Count];
+            // if there are no save commands, return an empty array
+            if (_saveCommands.Count == 0)
+            {
+                return [];
+            }
 
             // validate the save commands
             var validationResultTasks = _saveCommands
@@ -113,56 +116,18 @@ internal class BatchCommand<TInterface, TItem>(
 
             // acquire the save requests from each save command
             var acquireTasks = _saveCommands
-                .Select((sc, index) => {
-                    return (
-                        task: sc.AcquireAsync(requestContext, cancellationToken),
-                        index: index);
-                });
-
-            // wait for the acquire tasks to complete
-            // do not need to propagate the cancellation token
-            // each acquire task will handle the cancellation
-            Task.WaitAll(acquireTasks.Select(at => at.task), CancellationToken.None);
-
-            var isFaulted = acquireTasks.Any(at => at.task.IsFaulted);
-
-            if (isFaulted)
-            {
-                // something faulted
-
-                foreach (var (task, index) in acquireTasks)
-                {
-                    // if this task completed successfully, release the save command
-                    if (task.IsCompletedSuccessfully) _saveCommands[index].Release();
-
-                    // bad request: this faulted
-                    // failed dependency: something else faulted
-                    batchResults[index] = new BatchResult<TInterface, TItem>(
-                        httpStatusCode: task.IsFaulted
-                            ? HttpStatusCode.BadRequest
-                            : HttpStatusCode.FailedDependency,
-                        readResult: null);
-                }
-
-                return batchResults;
-            }
-
-            // throw if cancelled
-            cancellationToken.ThrowIfCancellationRequested();
-
-            // allocate the array of save requests
-            var requests = acquireTasks
-                .Select(at => at.task.Result)
+                .Select(sc => sc.AcquireAsync(requestContext, cancellationToken))
                 .ToArray();
 
-            // save the batch
-            var saveResults = await saveBatchAsyncDelegate(
-                partitionKey: null!,
-                requests: requests,
-                cancellationToken: cancellationToken);
+            // wait for the acquire tasks to complete
+            // do not propagate the cancellation token
+            // each acquire task will handle the cancellation
+            Task.WaitAll(acquireTasks, CancellationToken.None);
 
-            // handle the save requests
-            return HandleSaveResults(saveResults);
+            // check if any of the acquire tasks faulted
+            return acquireTasks.Any(at => at.IsFaulted)
+                ? AcquireTasksFaulted(acquireTasks)
+                : await AcquireTasksCompletedSuccessfully(acquireTasks, cancellationToken);
         }
         finally
         {
@@ -180,17 +145,13 @@ internal class BatchCommand<TInterface, TItem>(
         CancellationToken cancellationToken = default)
     {
         // ensure that only one operation that modifies the batch is in progress at a time
-        _semaphore.Wait();
+        await _semaphore.WaitAsync(cancellationToken);
 
-        // allocate the results
-        var validationResults = new ValidationResult[_saveCommands.Count];
+        // validate the save commands
+        var validationResultTasks = _saveCommands
+            .Select(sc => sc.ValidateAsync(cancellationToken));
 
-        // iterate over the save commands and validate
-        for (var index = 0; index < _saveCommands.Count; index++)
-        {
-            validationResults[index] = await _saveCommands[index]
-                .ValidateAsync(cancellationToken);
-        }
+        var validationResults = await Task.WhenAll(validationResultTasks);
 
         // release the exclusive lock
         _semaphore.Release();
@@ -199,13 +160,63 @@ internal class BatchCommand<TInterface, TItem>(
     }
 
     /// <summary>
-    /// Update and release the save commands
+    /// Build the batch results from the acquire tasks - faulted path.
     /// </summary>
-    /// <param name="saveResults">The save results returned from the save batch delegate.</param>
-    /// <returns>The array of <see cref="IBatchResult{TInterface}"/> with the items that were read.</returns>
-    private IBatchResult<TInterface>[] HandleSaveResults(
-        SaveResult<TInterface, TItem>[] saveResults)
+    /// <param name="acquireTasks">The acquire tasks from which to build the batch results.</param>
+    /// <returns>The array of <see cref="IBatchResult{TInterface}"/>.</returns>
+    private IBatchResult<TInterface>[] AcquireTasksFaulted(
+        Task<SaveRequest<TInterface, TItem>>[] acquireTasks)
     {
+        // allocate the array of batch results
+        var batchResults = new BatchResult<TInterface, TItem>[acquireTasks.Length];
+
+        for (var index = 0; index < acquireTasks.Length; index++)
+        {
+            // get the acquire task
+            var acquireTask = acquireTasks[index];
+
+            // if this task completed successfully, release the save command
+            if (acquireTask.IsCompletedSuccessfully) _saveCommands[index].Release();
+
+            // bad request: this faulted
+            // failed dependency: something else faulted
+            batchResults[index] = new BatchResult<TInterface, TItem>(
+                httpStatusCode: acquireTask.IsFaulted
+                    ? HttpStatusCode.BadRequest
+                    : HttpStatusCode.FailedDependency,
+                readResult: null);
+        }
+
+        return batchResults;
+    }
+
+    /// <summary>
+    /// Build the batch results from the acquire tasks - completed successfully path.
+    /// </summary>
+    /// <param name="acquireTasks">The acquire tasks from which to build the batch results.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+    /// <returns>The array of <see cref="IBatchResult{TInterface}"/>.</returns>
+    private async Task<IBatchResult<TInterface>[]> AcquireTasksCompletedSuccessfully(
+        Task<SaveRequest<TInterface, TItem>>[] acquireTasks,
+        CancellationToken cancellationToken)
+    {
+        // throw if cancelled
+        cancellationToken.ThrowIfCancellationRequested();
+
+        // allocate the array of save requests
+        var requests = acquireTasks
+            .Select(at => at.Result)
+            .ToArray();
+
+        // get the partition key from the first item
+        var partitionKey = requests.First().Item.PartitionKey;
+
+        // save the batch
+        var saveResults = await saveBatchAsyncDelegate(
+            partitionKey,
+            requests,
+            cancellationToken);
+
         // allocate the batch results
         var batchResults = new BatchResult<TInterface, TItem>[_saveCommands.Count];
 
@@ -219,7 +230,9 @@ internal class BatchCommand<TInterface, TItem>(
             var saveResult = saveResults[index];
 
             // update the save command and get the read result
-            var readResult = isCompletedSuccessfully ? saveCommand.Update(saveResult.Item!) : null;
+            var readResult = isCompletedSuccessfully
+                ? saveCommand.Update(saveResult.Item!)
+                : null;
 
             // release the save command
             saveCommand.Release();
